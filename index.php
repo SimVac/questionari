60a21a95c4f3b6a0a8e484e6c440652eec85a7ae
SimVac <simonevacca932@gmail.com>
2024-05-10 20:02:17 +0200

    Aggiunge il login

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
     require 'vendor/autoload.php';
-    //require_once 'conf/config.php';
+    require_once 'conf/config.php';
+    use Util\Authenticator;
 
     function page_refresh(){
-        echo "<meta http-equiv='refresh' content='0'>";
+        echo '<meta http-equiv=\'refresh\' content=\'0\'>';
         exit;
+    }
+
+    $user = Authenticator::getUser();
+
+    if ($user != null){
+        echo 'ciao';
     }
 
     $template = new League\Plates\Engine('templates', 'tpl');
