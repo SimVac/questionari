f25210728bcd3b2c353bdedf0e9a761f493bcd87
SimVac <simonevacca932@gmail.com>
2024-05-15 17:01:01 +0200

    modifica conf di esempio

--- conf/config.example.php
+++ conf/config.example.php
@@ -1,6 +1,21 @@
 <?php
 
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+
+const DEV = true;
+
 const DB_HOST = 'host';
 const DB_NAME = 'db_name';
 const DB_USER = 'user';
 const DB_PASSWORD = 'password';
+
+$email_config = [
+    'server'      => '',
+    'port'        => '',
+    'username'    => '',
+    'password'    => '',
+    'security'    => PHPMailer::ENCRYPTION_SMTPS,
+    'admin_email' => '',
+    'debug'       => (DEV) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF,
+];
