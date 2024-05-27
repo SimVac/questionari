c958fb5f5da5242f2c5acdddc6fada4b03496e7f
SimVac <103424187+SimVac@users.noreply.github.com>
2024-05-27 09:22:53 +0200

    Update config.example.php

--- conf/config.example.php
+++ conf/config.example.php
@@ -11,10 +11,10 @@ const DB_USER = 'user';
 const DB_PASSWORD = 'password';
 
 $email_config = [
-    'server'      => 'smtp.gmail.com',
-    'port'        => '465',
-    'username'    => 'survey.app.1357@gmail.com',
-    'password'    => 'ywqqjjyvbbpmuuug',
+    'server'      => '',
+    'port'        => '',
+    'username'    => '',
+    'password'    => '',
     'security'    => PHPMailer::ENCRYPTION_SMTPS,
     'admin_email' => '',
     'debug'       => (DEV) ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF,
